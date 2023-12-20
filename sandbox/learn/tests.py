1e4a25000ddb5a67d2586a4a2353ff75b07f164a
Chibuzo Kelechi <narutokelechi@gmail.com>
2023-12-20 12:30:44 +0100

    First test and results view

--- sandbox/learn/tests.py
+++ sandbox/learn/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+
+from .models import Question
+
 
 # Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(q_date=time)
+        self.assertIs(future_question.asked_recent(), True)
