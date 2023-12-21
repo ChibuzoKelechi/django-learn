fa1a8276544c31057b074fbaef62e450bec2344d
Chibuzo Kelechi <narutokelechi@gmail.com>
2023-12-21 04:58:13 +0100

    Basic Admin cusomization

--- sandbox/learn/admin.py
+++ sandbox/learn/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question, Answer
-admin.site.register(Question)
 admin.site.register(Answer)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Answer
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['query_text', 'q_date', 'asked_recent']
+    list_filter = ['q_date']
+    search_fields = ['query_text']
+    fieldsets = [
+        (None, {'fields': ['query_text']}),
+        ('Date Information', {'fields': ['q_date'], 'classes' : ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
--- sandbox/learn/models.py
+++ sandbox/learn/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
@@ -10,10 +11,17 @@ class Question(models.Model):
     def __str__(self):
         return self.query_text 
     
-    def asked_recent(self):
-        return self.q_date >= timezone.now() - datetime.timedelta(days=1)
+    @admin.display(
+        boolean=True, 
+        ordering='q_date',
+        description='Published recently?'
+    )
     
-
+    def asked_recent(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.q_date <= now
+    
+ 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
     choice_text = models.CharField(max_length = 200)
